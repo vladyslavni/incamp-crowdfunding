be2444d28b6de233675ff192468c0245dbe6cbe9
vladyslavni <vladyslavnik@tahometer.com>
2020-09-02 15:00:09 +0300

    DB: add: Created DB context for PostgreSQL

---
+++ CroudfundingDb.cs
@@ -0,0 +1,23 @@
+using Crowdfunding.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crowdfunding
+{
+    public class CrowdfudingContext : DbContext
+    {
+        public DbSet<User> Users {get; set;}
+        public DbSet<Team> Teams {get; set;}
+        public DbSet<Investment> Investments {get; set;}
+        public DbSet<Project> Projects {get; set;}
+
+        public CrowdfudingContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=CroudfundingDb;Username=postgres;Password=admin");
+        }
+    }
+}
