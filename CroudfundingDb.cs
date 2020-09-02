d8bcc3ecea8bf2e9b56342c3dbe8dec4e6bb9e28
vladyslavni <vladyslavnik@tahometer.com>
2020-09-02 17:05:44 +0300

    DataBase: fix: Changed DataBase name to correct one

--- CroudfundingDb.cs
+++ CroudfundingDb.cs
@@ -17,7 +17,7 @@ namespace Crowdfunding
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=CroudfundingDb;Username=postgres;Password=admin");
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=admin");
         }
     }
 }
