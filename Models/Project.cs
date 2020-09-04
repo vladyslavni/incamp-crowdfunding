2967f8b97b53a35360b4f3e6f9ac606a56f4527a
vladyslavni <vladyslavnik@tahometer.com>
2020-09-04 14:46:36 +0300

    Models: enhance: Added CollectedMoney field

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,4 +1,5 @@
 using Crowdfunding.Models.Enums;
+using System.Collections.Generic;
 
 namespace Crowdfunding.Models
 {
@@ -8,6 +9,7 @@ namespace Crowdfunding.Models
         public string Name {get; set;}
         public string Description {get; set;}
         public long Goal {get; set;}
+        public double CollectedMoney {get; set;}
         public Team Team {get; set;}
         public ProjectStatus Status {get; set;}
     }
