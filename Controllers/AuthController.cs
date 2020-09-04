d9eb9f9260fb948601e31d088b60327ffdf564aa
vladyslavni <vladyslavnik@tahometer.com>
2020-09-04 09:12:34 +0300

    2 Controllers: fix: Authentification methods fixed

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,46 +1,60 @@
-using System.Security.AccessControl;
+using System.Security.Claims;
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Crowdfunding.Models.Dto;
+using Crowdfunding.Models;
+using Crowdfunding.Models.Mappers;
 using Crowdfunding.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 
 namespace Crowdfunding.Controllers
 {
-    [Authorize]
+
     [ApiController]
     [Route("api/account")]
-    public class AuthController
+    public class AuthController : Controller
     {
         private UserService userService;
-        private IHttpContextAccessor accessor;
-        public AuthController(UserService userService, IHttpContextAccessor accessor)
+        private SignInManager<User> signInManager;
+        public AuthController(UserService userService, SignInManager<User> signInManager)
         {
             this.userService = userService;
-            this.accessor = accessor;
+            this.signInManager = signInManager;
         }
 
         [AllowAnonymous]
+        [ValidateAntiForgeryToken]
         [HttpPost("registration")]
-        public void RegisterUser(RegisterUserDto user)
-        { 
-            userService.CreateNew(user);
+        public void RegisterUser(RegisterUserDto userDto)
+        {
+            User user = RegisterUserMapper.Map(userDto);
+            signInManager.UserManager.CreateAsync(user, user.PasswordHash);
         }
 
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
         [HttpPost("login")]
-        public ActionResult LoginUser(LoginUserDto loginUser)
+        public void LoginUser(LoginUserDto loginUser)
         {
             var user = userService.GetByCredentials(loginUser);
-            if (user != null)  
+
+            if (user != null)
             {  
-                var httpContext = accessor.HttpContext;
-                httpContext.Session.SetString("UserID", user.Id.ToString());  
-                httpContext.Session.SetString("UserName", user.UserName);  
-                //TODO Переадресация
-                return null;
+                var result = signInManager.PasswordSignInAsync(user.UserName, loginUser.PasswordHash, false, false);
+                Console.WriteLine(result.Result.Succeeded);
             }  
-            //TODO Переадресация
-            return null;
+        }
+
+        [Authorize]
+        [HttpPost("logout")]
+        public void Logout()
+        {
+            signInManager.SignOutAsync();
         }
     }
 }
